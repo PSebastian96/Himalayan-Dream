b14e71f3cd6ef76d19442c8bfc00ef5c31352a89
PSebastian96 <123810890+PSebastian96@users.noreply.github.com>
2025-11-12 19:51:38 +0000

    JS current date added
    Add event listener to update year on page load

--- static/currentpage.js
+++ static/currentpage.js
@@ -13,3 +13,10 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const yearSpan = document.getElementById("year");
+    if (yearSpan) {
+      yearSpan.textContent = new Date().getFullYear();
+    }
+});
